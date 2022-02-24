b767700750f5bccc703ced64518a3a24201aaaf5
siyuanchen2020 <63905234+siyuanchen2020@users.noreply.github.com>
2022-02-25 00:08:33 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,28 @@
+from stable_baselines3 import dqn
+import numpy
+import gym
+import matplotlib.pyplot as plt
+import os
+
+
+'''
+runner = "xsim-runner.exe"
+model = "LawMcComasMOPs.xml"
+input = "input.txt"
+output = "output_Law.txt"
+'''
+
+#function for changing the input variable
+input_var = numpy.array([2, 3, 2, 2, 6, 7, 3])
+numpy.savetxt('input.txt', input_var)
+
+os.system("xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law2.txt")
+
+#xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt
+
+# check the output array
+with open('output_Law2.txt') as my_file:
+    # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
+    output_array = my_file.readlines()
+
+print(output_array)
